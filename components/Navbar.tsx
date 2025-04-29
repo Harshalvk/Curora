287b661abbdbeeec7499a80b916497b0c20a5951
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-29 17:23:47 +0530

    feat: add Navbar component with user authentication and mode toggle

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import { auth } from "@/auth";
+import Logo from "@/components/Logo";
+import { ModeToggle } from "@/components/ModeToggle";
+import React from "react";
+import { UserToogle } from "./UserAvatar";
+
+const Navbar = async () => {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    return <p>User not found</p>;
+  }
+
+  return (
+    <nav className="p-2 border-b flex items-center justify-between">
+      <Logo />
+      <div className="flex items-center gap-4">
+        <ModeToggle />
+        <UserToogle user={user} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
