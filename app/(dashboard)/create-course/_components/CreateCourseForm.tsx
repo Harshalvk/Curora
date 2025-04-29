fda107b899e0684b701c61ddaa9ad6177b819901
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-29 17:24:55 +0530

    feat: implement CreateCourseForm and integrate it into the CreateCourse page

---
+++ app/(dashboard)/create-course/_components/CreateCourseForm.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { CreateChapterSchema } from "@/schema/course";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { PlusCircle, Trash } from "lucide-react";
+import { motion, AnimatePresence } from "motion/react";
+
+const CreateCourseForm = () => {
+  const form = useForm<z.infer<typeof CreateChapterSchema>>({
+    resolver: zodResolver(CreateChapterSchema),
+    defaultValues: {
+      title: "",
+      units: ["", "", ""],
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof CreateChapterSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Enter the main topci of the course (e.g. 'Calculus')"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <AnimatePresence>
+          {form.watch("units").map((_, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: "auto" }}
+              exit={{ opacity: 0, height: 0 }}
+              transition={{
+                duration: 0.2,
+                ease: "easeInOut",
+              }}
+            >
+              <FormField
+                control={form.control}
+                name={`units.${index}`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Unit {index + 1}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter the subtopic of the course"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+
+        <div className="flex items-center justify-center">
+          <Separator className="flex-[1]" />
+          <div className="mx-4 space-x-2">
+            <Button
+              type="button"
+              variant={"secondary"}
+              onClick={() => {
+                form.setValue("units", [...form.watch("units"), ""]);
+              }}
+            >
+              Add Unit <PlusCircle className="h-4 w-4 text-green-500" />
+            </Button>
+            <Button
+              type="button"
+              variant={"secondary"}
+              onClick={() => {
+                form.setValue("units", [...form.watch("units").slice(0, -1)]);
+              }}
+            >
+              Remove Unit <Trash className="h-4 w-4 text-red-500" />
+            </Button>
+          </div>
+          <Separator className="flex-[1]" />
+        </div>
+        <Button
+          type="submit"
+          variant={"secondary"}
+          className="border w-full md:w-fit"
+        >
+          Let&apos;s go!
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default CreateCourseForm;
---
+++ app/(dashboard)/create-course/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Info } from "lucide-react";
+import CreateCourseForm from "./_components/CreateCourseForm";
+
+const CreateCourse = () => {
+  return (
+    <div>
+      <h1 className="mb-2 font-semibold sm:text-xl md:text-2xl">
+        Create Course
+      </h1>
+      <Alert className="max-w-fit">
+        <Info className="h-4 w-4" />
+        <AlertTitle>Important!</AlertTitle>
+        <AlertDescription>
+          Provide a course title or the topic you want to explore. Then, list
+          the units detailing what you want to learn. Our AI will create a
+          tailored course for you!
+        </AlertDescription>
+      </Alert>
+      <div className="mt-3 border p-4 rounded-md">
+        <CreateCourseForm />
+      </div>
+    </div>
+  );
+};
+
+export default CreateCourse;
