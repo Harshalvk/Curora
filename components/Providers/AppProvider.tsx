f3a776f8d7325797931203d26aec8a5deb2ef908
Harshalvk <harshalvkhobragade@gmail.com>
2025-05-10 22:51:31 +0530

    update: install and setup Tanstack react-query

--- components/Providers/AppProvider.tsx
+++ components/Providers/AppProvider.tsx
@@ -1,16 +1,25 @@
+"use client";
+
 import React, { ReactNode } from "react";
 import { ThemeProvider } from "./ThemeProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "../ui/sonner";
+
+const queryclient = new QueryClient();
 
 const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <ThemeProvider
-      attribute={"class"}
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryclient}>
+      <ThemeProvider
+        attribute={"class"}
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
