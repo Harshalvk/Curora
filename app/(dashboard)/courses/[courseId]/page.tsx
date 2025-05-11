0a18e36abfc649f78fc125f80f09820b1684d9ba
Harshalvk <harshalvkhobragade@gmail.com>
2025-05-11 23:05:47 +0530

    feat: create CoursePage component for displaying course details and chapters confirmation

---
+++ app/(dashboard)/courses/[courseId]/page.tsx
@@ -0,0 +1,59 @@
+import { auth } from "@/auth";
+import AlertMessage from "@/components/Alert";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import React from "react";
+import ConfirmChapters from "./_components/ConfirmChapters";
+
+type Props = {
+  params: {
+    courseId: string;
+  };
+};
+
+const CoursePage = async ({ params }: Props) => {
+  await params;
+
+  const session = await auth();
+
+  if (!session?.user) {
+    return redirect("/gallery");
+  }
+
+  const course = await prisma.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      units: {
+        include: {
+          chapters: true,
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/create");
+  }
+
+  return (
+    <div className="p-4">
+      <div>
+        <span className="text-muted-foreground text-sm">Course name</span>
+        <h1 className="capitalize font-bold text-xl sm:text-2xl leading-5">
+          {course.name}
+        </h1>
+      </div>
+      <div className="max-w-4xl mx-auto mt-4">
+        <AlertMessage
+          title="Important!"
+          description="Below are the generated chapters for each of your units. Look over them & then click on the button to confirm and continue."
+        />
+        <ConfirmChapters course={course} />
+      </div>
+    </div>
+  );
+};
+
+export default CoursePage;
